16a6924d9cb1b6400fe0460c54289f8dff0d9a31
ilyazavgorodniy7 <119864808+ilyazavgorodniy7@users.noreply.github.com>
2023-03-14 17:00:19 +0300

    Update form.php

--- lab3/PHP/form.php
+++ lab3/PHP/form.php
@@ -1,6 +1,6 @@
 <form action="" method="POST">
-  <input name="fio"  placeholder="fio">
-  <input name="mail" placeholder="mail">
-  <input name="year" placeholder="year">
+  <input name="fio"  placeholder="fio"/>
+  <input name="mail" placeholder="mail"/>
+  <input name="year" placeholder="year"/>
   <input type="submit" value="ok" />
 </form>
