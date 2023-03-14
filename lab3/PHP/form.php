8f9579a3f7ba6b358aabda8da8f084e2b3c39c89
ilyazavgorodniy7 <119864808+ilyazavgorodniy7@users.noreply.github.com>
2023-03-14 22:41:18 +0300

    Update form.php

--- lab3/PHP/form.php
+++ lab3/PHP/form.php
@@ -11,7 +11,6 @@
   <label>
     <input type="radio" checked="checked" name="man" value="man">man
   </label>
-   </br>
    <label>
   <input type="radio" checked="checked" name="woman" value="woman">woman
    </label>
@@ -21,15 +20,12 @@
    <label>
    <input type="radio" checked="checked" name="r" value="2">2-4
    </label>
-   </br>
    <label>
    <input type="radio" name="r" value="5">5-7
    </label>
-   </br>
    <label>
    <input type="radio" name="r" value="8">8-10
    </label>
-   </br>
    <label>
   <input type="submit" value="ok" />
 </form>
