bf050ae8d1b7d8a5d0a7a89dee3a2672f9b0c4a0
ilyazavgorodniy7 <119864808+ilyazavgorodniy7@users.noreply.github.com>
2023-03-14 22:37:37 +0300

    Update form.php

--- lab3/PHP/form.php
+++ lab3/PHP/form.php
@@ -1,6 +1,35 @@
 <form action="" method="POST">
   <input name="fio"  placeholder="fio"/>
+  </br>
   <input name="mail" placeholder="mail"/>
+  </br>
   <input name="year" placeholder="year"/>
+  </br>
+  <?php
+  printf('Gender :')
+  ?>
+  <label>
+    <input type="radio" checked="checked" name="man" value="man">man
+  </label>
+   </br>
+   <label>
+  <input type="radio" checked="checked" name="woman" value="woman">woman
+   </label>
+  <?php
+  printf('Count_limb :')
+  ?>
+   <label>
+   <input type="radio" checked="checked" name="r" value="2">2-4
+   </label>
+   </br>
+   <label>
+   <input type="radio" name="r" value="5">5-7
+   </label>
+   </br>
+   <label>
+   <input type="radio" name="r" value="8">8-10
+   </label>
+   </br>
+   <label>
   <input type="submit" value="ok" />
 </form>
