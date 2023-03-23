2aa6cf8b4c7b1fa50a29b261d6956e938649b2e4
ilyazavgorodniy7 <119864808+ilyazavgorodniy7@users.noreply.github.com>
2023-03-23 19:18:55 +0300

    Create form.php

---
+++ lab3/form.php
@@ -0,0 +1,80 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+<form action="index.php" method="POST">
+    <label>
+    <?php
+      printf('Имя:');
+    ?>
+    <br>
+    <input name="name" placeholder="name">
+    </label>
+    <label>
+    <?php
+      printf('E-mail:');
+    ?>
+    <br>
+    <input name="email" type="email" placeholder="email">
+    </label>
+    <label>
+    <?php
+      printf('Год рождения:');
+    ?>
+    <br>
+    <input name="year" placeholder="year">
+    </label>
+    <label>
+    <?php
+      printf('Гендер:');
+    ?>
+    <br>
+    <?php
+      printf('Мужской');
+    ?>
+    <input type="radio" name="gender" value="1">
+    <?php
+      printf('Женский');
+    ?>
+    <input type="radio" name="gender" value="2">
+    </label>
+    <label>
+    <?php
+      printf('Количество конечностей: ');
+      printf('2-4');
+    ?>
+    <input type="radio" value="1" name="count_limb">
+    <?php
+      printf('5-7');
+    ?>
+    <input type="radio" value="2" name="count_limb">
+    <?php
+      printf('8-10');
+    ?>
+    <input type="radio" value="3" name="count_limb">
+    </label>
+    <label>
+    <?php
+      printf('Сверхспособности:');
+    ?>
+    <br>
+    <select name="abilities[]" multiple="multiple"> 
+      <option value="1">Бессмертие</option>
+      <option value="2">Проходить сквозь стены</option>
+      <option value="3">Левитация</option>
+    </select>
+    </label>
+    <label>
+    <?php
+      printf('О себе:');
+    ?>
+    <br>
+    <textarea name="biography" placeholder="about me"></textarea>
+    </label>
+    <label>
+    <?php
+      printf('С контрактом ознакомлен(-а)');
+    ?>
+    <input type="checkbox" name="checked" value="on">
+    </label>
+    <label>
+    <input type="submit" value="ok" class="button"/>
+    </label>
+  </form>
