f60c2caa7ef3adb9e2ee2b6b6f918ab79150a646
ilyazavgorodniy7 <119864808+ilyazavgorodniy7@users.noreply.github.com>
2023-03-23 19:48:31 +0300

    Update index.php

--- lab3/index.php
+++ lab3/index.php
@@ -1,6 +1,4 @@
 <?php
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
@@ -11,55 +9,49 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     // Если есть параметр save, то выводим сообщение пользователю.
     print('Спасибо, результаты сохранены.');
   }
-
-  // Завершаем работу скрипта.
   exit();
 }
 include('form.php');
 // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 
-// Проверяем ошибки.
 $bioreg = "/^\s*\w+[\w\s\.,-]*$/";
 $reg = "/^\w+[\w\s-]*$/";
 $mailreg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
-$list_super_power = array('1','2','3');
+$list_abilities = array('1','2','3');
 
 $errors = FALSE;
 if(empty($_POST['name'])){
-	print_r('Заполните имя');
+	print_r('Заполните Имя!');
 	exit();
 }
 if(empty($_POST['email'])){
-	print_r('Заполните email');
+	print_r('Заполните email!');
 	exit();
 }
 if(empty($_POST['year'])){
-	print_r('Заполните год');
+	print_r('Заполните год!');
 	exit();
 }
 if(empty($_POST['biography'])){
-	print_r('Заполните биографию');
+	print_r('Заполните биографию!');
 	exit();
 }
 if(empty($_POST['gender'])){
-	print_r('Заполните пол');
+	print_r('Заполните пол!');
 	exit();
 }
 if(empty($_POST['count_limb'])){
-	print_r('Заполните кол-во конечностей');
+	print_r('Заполните кол-во конечностей!');
 	exit();
 }
-
 if(!preg_match($mailreg,$_POST['email'])){
         print_r('Неверный формат email');
 	exit();
 }
-
 if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
- 	print('Неверно указан год');
+ 	print('Неверно указан год.<br/>');
 	exit();
 }
-
 if(!preg_match($reg,$_POST['name'])){
 	print_r('Неверный формат имени');
 	exit();
@@ -69,29 +61,21 @@ if(!preg_match($bioreg,$_POST['biography'])){
 	exit();
 }
 foreach($_POST['super_power'] as $checking){
-	if(array_search($checking,$list_super_power)=== false){
+	if(array_search($checking,$list_abilities)=== false){
 		print_r('Неверный формат суперсил');
 		exit();
 	}
 }
 
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
 if ($errors) {
-  // При наличии ошибок завершаем работу скрипта.
   exit();
 }
 
-// Сохранение в базу данных.
+$user = 'u52805';
+$pass = '5816061';
+$db = new PDO('mysql:host=localhost;dbname=u52826', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-$user = 'u52805'; // Заменить на ваш логин uXXXXX
-$pass = '5816061'; // Заменить на пароль, такой же, как от SSH
-$db = new PDO('mysql:host=localhost;dbname=u52805', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-
-// Подготовленный запрос. Не именованные метки.
 try {
   $stmt = $db->prepare("INSERT INTO person SET name = ?,email= ?, year= ?, gender= ?, count_limb= ?, biography= ?,checked= ?");
   $stmt->execute([$_POST['name'],$_POST['email'],$_POST['year'],$_POST['gender'],$_POST['count_limb'],$_POST['biography'],$_POST['checked']]);
@@ -99,8 +83,8 @@ try {
   $id = $db->lastInsertId();
   $sppe= $db->prepare("INSERT INTO super_power SET power_id=:power, person_id=:person");
   $sppe->bindParam(':person', $id);
-  foreach($_POST['super_power']  as $power){
-	$sppe->bindParam(':power', $power);
+  foreach($_POST['super_power']  as $ability){
+	$sppe->bindParam(':power', $ability);
 	if($sppe->execute()==false){
 	  print_r($sppe->errorCode());
 	  print_r($sppe->errorInfo());
@@ -112,26 +96,4 @@ catch(PDOException $e){
   print('Error : ' . $e->getMessage());
   exit();
 }
-
-
-//  stmt - это "дескриптор состояния".
- 
-//  Именованные метки.
-//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
-//$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
- 
-//Еще вариант
-/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
-*/
-
-// Делаем перенаправление.
-// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
-// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
 header('Location: ?save=1');
