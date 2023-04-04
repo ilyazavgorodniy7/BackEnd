dfc7566bbf0465708a788dd5c5e74e6b6e650c24
ilyazavgorodniy7 <119864808+ilyazavgorodniy7@users.noreply.github.com>
2023-04-04 15:06:10 +0300

    Update index.php

--- lab5/index.php
+++ lab5/index.php
@@ -173,7 +173,6 @@ else {
 else {
     // Генерируем уникальный логин и пароль.
     // TODO: сделать механизм генерации, например функциями rand(), uniquid(), md5(), substr().
-    $login = $db->lastInsertId();;
     $login = '';
     $password = '';
     $arr = array('a','b','c','d','e','f',
