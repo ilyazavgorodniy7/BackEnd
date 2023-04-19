65219f2b87b0672c6c410b9c7c4defb7b55431e2
ilyazavgorodniy7 <119864808+ilyazavgorodniy7@users.noreply.github.com>
2023-04-19 19:55:04 +0300

    Create login.php

--- lab5/login.php
+++ lab5/login.php
@@ -1,1 +1,64 @@
+<html>
+<header>
+  <link rel="stylesheet" href="style.css" type="text/css">
+</header>
+  <?php
+  header('Content-Type: text/html; charset=UTF-8');
+  session_start();
+  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_SESSION['login'])) {
+    header('Location: index.php');
+    }else{
+  ?>
+  <body>
+    <form action="login.php" method="post">
+      <br>Логин
+      <input name="login" placeholder="login">
+      Пароль
+      <input name="password" type="password" placeholder="password">
+      <input type="submit" value="Войти" />
+    </form>
+  </body>
+</html>
+<?php
+  }
+}
+// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
+else {
+  $login=$_POST['login'];
+  $password=$_POST['password'];
+  $uid=0;
+  $error=TRUE;
+  $user = 'u52826';
+  $pass = '4927417';
+  $db1 = new PDO('mysql:host=localhost;dbname=u52826', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  if(!empty($login) and !empty($password)){
+    try{
+      $chk=$db1->prepare("SELECT * FROM user_info WHERE login=?");
+      $chk->bindParam(1,$login);
+      $chk->execute();
+      $username=$chk->fetchALL();
+	  print($username[0]['password']);
+      if(password_verify($password,$username[0]['password'])){
+        $uid=$username[0]['id'];
+        $error=FALSE;
+      }
+    }
+    catch(PDOException $e){
+      print('Error : ' . $e->getMessage());
+      exit();
+    }
+  }
+  if($error==TRUE){
+    print('<br>Неверный логин/пароль. <br> Создайте нового <a href="index.php" style="color:black;">пользователя</a> или попытайтесь <a href="login.php"style="color:black;">войти</a> снова');
+    session_destroy();
+    exit();
+  }
+  // Если все ок, то авторизуем пользователя.
+  $_SESSION['login'] = $login;
+  // Записываем ID пользователя.
+  $_SESSION['uid'] = $uid;
+  // Делаем перенаправление.
+  header('Location: index.php');
+}
 
